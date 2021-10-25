c9db94760615e5d268c973c004ee015f31a62ee4
54895 <54895@etu.he2b.be>
2021-10-25 22:40:35 +0200

    Update tests.

--- tests/EtherCrowd/test_fund_crowdsale.py
+++ tests/EtherCrowd/test_fund_crowdsale.py
@@ -1,9 +1,13 @@
 from brownie.test import given, strategy
-from brownie import accounts
+from brownie import accounts, reverts
 import pytest
 
-
-def setup():
+@pytest.fixture(autouse=True)
+def setup(fn_isolation, ethercrowd, accounts):
+    """
+    Isolation setup fixture.
+    This ensures that each test runs against the same base environment.
+    """
     goal_amount = 10
     title = "Test crowdsale"
     slogan = "Test slogan"
@@ -26,47 +30,65 @@ def setup():
         start_date,
         end_date,
         {'from': accounts[0], "value": 1}
-        )  # TODO: global test fee variable
+    )  # TODO: global test fee variable
 
 
-# @pytest.mark.parametrize('user', accounts[0:4])
 def test_fund_existing_crowdsale(ethercrowd, accounts):
     # Init
     crowdId = 0
-    expected = 10 #montant qui va etre mit
+    expected = 10  # montant qui va etre mit
 
     # Call
-    fund(0)
-    result = ethercrowd.getFundRaise(crowdId) #montant qui a ete mit
+    ethercrowd.fund(0, {'from': accounts[0], "value": 10})
+    result = ethercrowd.getInvestedFunds(crowdId)  # montant qui a ete mit
 
-    # Assert
-    assert expected == result
-
-
-# @pytest.mark.parametrize('user', accounts[0:4])
-def test_fund_non_existing_crowdsale(ethercrowd, accounts):
-    # Init
-    crowdId = -404
-    expected = 0 #= #montant qui va etre mit
-
-    # Call
-    fund(crowdId)
-
-    resultInvestedCrowdsaleList # verifie si la crowd a été ajouté dans ses crowds investit
-    result = ethercrowd.getFundRaise(crowdId)#= #montant qui a ete mit
     # Assert
     assert expected == result
     
-    # @pytest.mark.parametrize('user', accounts[0:4])
+
+
 def test_fund_existing_crowdsale_no_money(ethercrowd, accounts):
     # Init
     crowdId = 0
+    expected = 0  # = #montant qui va etre mit
+
+    # Call
+    with reverts("No value sent."):
+        ethercrowd.fund(crowdId, {'from': accounts[0], "value": 0})
+
+    # resultInvestedCrowdsaleList # verifie si la crowd a été ajouté dans ses crowds investit
+    result = ethercrowd.getInvestedFunds(crowdId)  # = #montant qui a ete mit
+
+    # Assert
+    assert expected == result
+
+
+def test_fund_non_existing_crowdsale(ethercrowd, accounts):
+    # Init
+    crowdId = 404
+    expected = 0  # = #montant qui va etre mit
+
+    # Call
+    with reverts("Crowd does not exist."):
+        ethercrowd.fund(crowdId, {'from': accounts[0], "value": 10})
+
+
+# TODO when Jona will finish the isActive implementation
+"""
+def test_fund_non_active_crowdsale(ethercrowd, accounts):
+    # Init
+    crowdId = 1
     expected = 0 #= #montant qui va etre mit
 
     # Call
-    fund(crowdId)
+    with reverts("Crowd is not active."):
+        ethercrowd.fund(crowdId, {'from': accounts[0], "value": 10})
 
-    resultInvestedCrowdsaleList # verifie si la crowd a été ajouté dans ses crowds investit
-    result = ethercrowd.getFundRaise(crowdId)#= #montant qui a ete mit
+    #resultInvestedCrowdsaleList # verifie si la crowd a été ajouté dans ses crowds investit
+    result = ethercrowd.getInvestedFunds(crowdId)#= #montant qui a ete mit
+    
     # Assert
     assert expected == result
+"""
+
+
