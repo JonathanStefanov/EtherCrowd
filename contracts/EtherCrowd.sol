2205c415d98fc4655a30e9e2f741d2fb1530d07d
54895 <54895@etu.he2b.be>
2021-11-02 21:31:32 +0100

    Update getProject function.

--- contracts/EtherCrowd.sol
+++ contracts/EtherCrowd.sol
@@ -2,21 +2,24 @@ pragma solidity ^0.8.0;
 import "../interfaces/KeeperCompatibleInterface.sol";
 
 contract EtherCrowd is KeeperCompatibleInterface {
-
-    uint private currentId;
+    uint256 private currentId;
     address admin;
 
-    uint public checkInterval;
-    uint public lastCheck;
+    uint256 public checkInterval;
+    uint256 public lastCheck;
 
-    mapping(uint => Project) private idToProject;
-    mapping(uint => mapping( address => uint)) private idToBalanceOfContributors; // crowdsale id,  user address to amount invested by user address TODO: balance of function
-    mapping( address => uint[] ) private addressToListOfProjects; // user address to list of crowdsales ids he is invested in
+    mapping(uint256 => Project) private idToProject;
 
-    uint public fee; //TODO: implement change fe function
+    // crowdsale id,  user address to amount invested by user address TODO: balance of function
+    mapping(uint256 => mapping(address => uint256))
+        private idToBalanceOfContributors;
 
+    // user address to list of crowdsales ids he is invested in
+    mapping(address => uint256[]) private addressToListOfProjects;
 
-    constructor(uint _fee, uint _checkInterval) {
+    uint256 public fee; //TODO: implement change fe function
+
+    constructor(uint256 _fee, uint256 _checkInterval) {
         currentId = 0;
         admin = msg.sender;
         fee = _fee;
@@ -24,35 +27,28 @@ contract EtherCrowd is KeeperCompatibleInterface {
         lastCheck = 0;
     }
 
-    struct Project{
+    struct Project {
         bool initialized;
-
         address owner;
-        uint id;
-
+        uint256 id;
         string title;
         string slogan;
         string description;
-
         string websiteUrl;
         string thumbnailUrl;
         string videoUrl;
-
-        uint currentAmount;
-        uint goalAmount;
-
+        uint256 currentAmount;
+        uint256 goalAmount;
         //TODO implement themes
 
-        uint startDate; // TODO: implement
-        uint endDate;
+        uint256 startDate; // TODO: implement
+        uint256 endDate;
         bool isActive; // TODO ENUM: ISACTIVE; WILL BE ACTIVE; IS ENDED
-
         address[] contributors;
     }
 
-    
     function createProject(
-        uint _goalAmount,
+        uint256 _goalAmount,
         string memory _title,
         string memory _slogan,
         string memory _websiteUrl,
@@ -60,82 +56,86 @@ contract EtherCrowd is KeeperCompatibleInterface {
         string memory _thumbnailUrl,
         string memory _description,
         //  no need to specify start date, it is the time at which the user calls the function
-        uint _endDate
-
-    ) external payable{
+        uint256 _endDate
+    ) external payable {
         require(_endDate > 0, "End date has to be after start date");
         //require(idToCrowdsale[currentId].id != 0, "Crowdsale id already exists"); // if two crowdsales are created at the same time and thus get the same id
 
-        // Payment to list on EtherCrowd 
+        // Payment to list on EtherCrowd
         // Person has to send the exact fee with the function call, otherwise the transaction will be reverted
-        if(msg.value != fee){
+        if (msg.value != fee) {
             revert("Incorrect value");
         }
-
 
         address[] memory _contributors; // creating a empty array
 
         idToProject[currentId] = Project(
-                    true, // Crowdsale is initialized
-                    msg.sender,
-                    currentId, // is it necessary to repead the id here?
-                    _title,
-                    _slogan,
-                    _description,
-                    _websiteUrl,
-                    _thumbnailUrl,
-                    _videoUrl,
-                    0, // current amount 0 
-                    _goalAmount,
-                    block.timestamp, // start date
-                    block.timestamp + _endDate,
-                    true, // isActive set to true by default, will not be the case later
-                    _contributors
-                );
+            true, // Crowdsale is initialized
+            msg.sender,
+            currentId, // is it necessary to repead the id here?
+            _title,
+            _slogan,
+            _description,
+            _websiteUrl,
+            _thumbnailUrl,
+            _videoUrl,
+            0, // current amount 0
+            _goalAmount,
+            block.timestamp, // start date
+            block.timestamp + _endDate,
+            true, // isActive set to true by default, will not be the case later
+            _contributors
+        );
 
         currentId++; // incrementing the current id
 
         // maybe returning the id of the crowdsale?
     }
 
-    function getProject(uint _id) external view returns(
-        address owner
-            )
-        {
-        require(idToProject[_id].initialized == true, "No project with this id");
-        Project memory project = idToProject[_id];
 
-        //TODO: add all crowdsale arguments
-
-        return(
-            project.owner
-            );
-
-
+    modifier projectExist(uint256 _id) { 
+        require(idToProject[_id].initialized == true, "No project with this id"); 
+        _; 
     }
 
+    function getProject(uint256 _id) external view projectExist(_id) returns (Project memory) { 
+        Project memory project = idToProject[_id];
 
+        return (project);
+    }
 
     // ChainLink UpKeep part, maybe putting it in another file?
 
-    function checkUpkeep(bytes calldata /*checkData*/) external override view returns (bool upkeepNeeded, bytes memory /*performData*/) {
+    function checkUpkeep(
+        bytes calldata /*checkData*/
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /*performData*/
+        )
+    {
         // This function will be executed Off chain by the Keeper node, this is why the computation is done here in order to save on gas fees
         upkeepNeeded = (block.timestamp - lastCheck) > checkInterval;
         // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
     }
 
-    function performUpkeep(bytes calldata  /*performData*/ ) override external {
+    function performUpkeep(
+        bytes calldata /*performData*/
+    ) external override {
         lastCheck = block.timestamp;
         checkProjects();
 
         // We don't use the performData in this example. The performData is generated by the Keeper's call to your checkUpkeep function
-    }   
+    }
 
     function checkProjects() private {
-        for(uint i=0; i < currentId; i++){
+        for (uint256 i = 0; i < currentId; i++) {
             // looping through all the crowdsales
             Project memory project = idToProject[i];
-            if(block.timestamp >= project.endDate){
+            if (block.timestamp >= project.endDate) {
                 // It means the crowdsale has ended
                 endProject(project);
             }
@@ -145,45 +145,44 @@ contract EtherCrowd is KeeperCompatibleInterface {
     function endProject(Project memory _project) private {
         // TODO: Emit events
         // Project has ended, refund the money or give it to the owner according to the situation
-        if(_project.currentAmount >= _project.goalAmount){
+        if (_project.currentAmount >= _project.goalAmount) {
             // The crowdsale is successful, we can transfer the money to the owner
             payable(_project.owner).transfer(_project.currentAmount);
-            
-        }
-        else{
+        } else {
             // refunding the investors
             refund(_project);
         }
-
     }
 
     function refund(Project memory _project) private {
-            for (uint i=0; i< _project.contributors.length ; i++) {
-                address contributorAddress = _project.contributors[i];
-                uint refundAmount = idToBalanceOfContributors[_project.id][contributorAddress];
+        for (uint256 i = 0; i < _project.contributors.length; i++) {
+            address contributorAddress = _project.contributors[i];
+            uint256 refundAmount = idToBalanceOfContributors[_project.id][
+                contributorAddress
+            ];
 
-                payable(contributorAddress).transfer(refundAmount);
-            }
-
+            payable(contributorAddress).transfer(refundAmount);
+        }
     }
 
-
-
-    
     /** 
     Function fund, fund a crowd,
     takes a crowdid in parameter
     */
-    function fund(uint _projectId) payable external {
+    function fund(uint256 _projectId) external payable {
         require(msg.value > 0, "No value sent.");
-        require(idToProject[_projectId].initialized, "Project does not exist."); 
+        require(idToProject[_projectId].initialized, "Project does not exist.");
         require(idToProject[_projectId].isActive, "Project is not active");
 
         addressToListOfProjects[msg.sender].push(_projectId);
         idToBalanceOfContributors[_projectId][msg.sender] += msg.value;
     }
-    
-    function getInvestedFunds(uint _projectId) public view returns (uint balance) {
+
+    function getInvestedFunds(uint256 _projectId)
+        public
+        view
+        returns (uint256 balance)
+    {
         require(idToProject[_projectId].initialized, "Project does not exist.");
         return idToBalanceOfContributors[_projectId][msg.sender];
     }
