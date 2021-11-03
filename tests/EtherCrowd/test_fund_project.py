370d4af55c9e25b4599736c4ddb345f270c847de
54895 <54895@etu.he2b.be>
2021-11-03 17:56:22 +0100

    Update setup of test_fund_project.py

--- tests/EtherCrowd/test_fund_project.py
+++ tests/EtherCrowd/test_fund_project.py
@@ -2,32 +2,23 @@ from brownie.test import given, strategy
 from brownie import accounts, reverts
 import pytest
 
+
 @pytest.fixture(autouse=True)
 def setup(fn_isolation, ethercrowd, accounts):
-    """
-    Isolation setup fixture.
-    This ensures that each test runs against the same base environment.
-    """
-    # Maybe define expected and result for more visibility
     goal_amount = 10
-    title = "Test project"
-    slogan = "Test slogan"
-    website_url = "https://ethereum.org"
-    video_url = website_url
-    thumbnail_url = website_url
-    description = "Test description"
-    end_date = 120
+    title = "My awesome project"
+    slogan = "My awesome slogan"
+    website_url = "https://myAwesomeWebsiteUrl.org"
+    video_url = "https://myAwesomeVideoUrl.org"
+    thumbnail_url = "https://myAwesomeThumbnailUrl.org"
+    description = "My awesome description"
+    end_date = 3600
 
-    ethercrowd.createProject(
-        goal_amount,
-        title,
-        slogan,
-        website_url,
-        video_url,
-        thumbnail_url,
-        description,
-        end_date,
-         {'from': accounts[0], "value": 1}) # TODO: global test fee variable
+    ethercrowd.createProject(goal_amount, title, slogan, website_url,
+                             video_url, thumbnail_url, description, end_date, {
+                                 'from': accounts[0],
+                                 "value": 1
+                             })
 
 
 def test_fund_existing_project(ethercrowd, accounts):
