fdfc22a9c77ade531b4d487f4b4fcdf41e10a260
54895 <54895@etu.he2b.be>
2021-11-03 15:49:02 +0100

    Add new tests to getProjects function.

--- tests/EtherCrowd/test_get_project.py
+++ tests/EtherCrowd/test_get_project.py
@@ -2,6 +2,7 @@ from brownie.test import given, strategy
 from brownie import accounts, reverts
 import pytest
 
+
 @pytest.fixture(autouse=True)
 def setup(fn_isolation, ethercrowd, accounts):
     goal_amount = 10
@@ -13,20 +14,15 @@ def setup(fn_isolation, ethercrowd, accounts):
     description = "My awesome description"
     end_date = 3600
 
-    ethercrowd.createProject(
-        goal_amount,
-        title,
-        slogan,
-        website_url,
-        video_url,
-        thumbnail_url,
-        description,
-        end_date,
-         {'from': accounts[0], "value": 1}) # TODO: global test fee variable
+    ethercrowd.createProject(goal_amount, title, slogan, website_url,
+                             video_url, thumbnail_url, description, end_date, {
+                                 'from': accounts[0],
+                                 "value": 1
+                             })
 
 
 def test_get_existing_project(ethercrowd, accounts):
-     # Init
+    # Init
     projectId = 0
     expected = True
 
@@ -42,7 +38,7 @@ def test_get_existing_project(ethercrowd, accounts):
 
 
 def test_get_non_existing_project(ethercrowd, accounts):
-     # Init
+    # Init
     projectId = 404
     expected = False
 
@@ -53,6 +49,41 @@ def test_get_non_existing_project(ethercrowd, accounts):
         project = ethercrowd.getProject(projectId)
 
     result = project != None
+
+    # Assert
+    assert expected == result
+
+
+#TODO check why if a change msg.value to another value -> error 
+def test_get_all_projects(ethercrowd, accounts):
+    # Init
+
+    ethercrowd.createProject(10, "Your awesome project", "Your awesome slogan",
+                             "https://yourAwesomeWebsiteUrl.org",
+                             "https://yourAwesomeVideoUrl.org",
+                             "https://yourAwesomeThumbnailUrl.org",
+                             "Your awesome description", 1800, {
+                                 'from': accounts[1],
+                                 "value": 1
+                             })
+
+    ethercrowd.createProject(10, "His awesome project", "His awesome slogan",
+                             "https://hisAwesomeWebsiteUrl.org",
+                             "https://hisAwesomeVideoUrl.org",
+                             "https://hisAwesomeThumbnailUrl.org",
+                             "His awesome description", 900, {
+                                 'from': accounts[2],
+                                 "value": 1
+                             })
+
+    expected = 3
+
+    project = None
+
+    # Call
+    projects = ethercrowd.getProjects()
+
+    result = len(projects)
 
     # Assert
     assert expected == result
