512de2046a4c5523abee79be42d4633059ca7358
54895 <54895@etu.he2b.be>
2021-11-03 17:58:56 +0100

    Add new test file for refund function.

---
+++ tests/EtherCrowd/test_refund_contributors.py
@@ -0,0 +1,16 @@
+@pytest.fixture(autouse=True)
+def setup(fn_isolation, ethercrowd, accounts):
+    goal_amount = 10
+    title = "My awesome project"
+    slogan = "My awesome slogan"
+    website_url = "https://myAwesomeWebsiteUrl.org"
+    video_url = "https://myAwesomeVideoUrl.org"
+    thumbnail_url = "https://myAwesomeThumbnailUrl.org"
+    description = "My awesome description"
+    end_date = 3600
+
+    ethercrowd.createProject(goal_amount, title, slogan, website_url,
+                             video_url, thumbnail_url, description, end_date, {
+                                 'from': accounts[0],
+                                 "value": 1
+                             })
