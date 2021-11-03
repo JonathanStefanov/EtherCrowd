c8d08701bb22a003ad0fcab54a522f8dbf0789f8
54895 <54895@etu.he2b.be>
2021-11-03 19:43:16 +0100

    Add more comments.

--- tests/EtherCrowd/test_refund_contributors.py
+++ tests/EtherCrowd/test_refund_contributors.py
@@ -14,3 +14,25 @@ def setup(fn_isolation, ethercrowd, accounts):
                                  'from': accounts[0],
                                  "value": 1
                              })
+    
+    ethercrowd.fund(0, {'from': accounts[0], "value": 5})
+    ethercrowd.fund(0, {'from': accounts[1], "value": 10})
+    ethercrowd.fund(0, {'from': accounts[2], "value": 15})
+    ethercrowd.fund(0, {'from': accounts[3], "value": 20})
+
+
+def test_refund_contributors(ethercrowd, accounts):
+    # Init
+    projectId = 0
+    nbOfContributors = 4
+    expected = 0
+
+    # Call
+    ethercrowd.refund(0)
+    result = True
+     
+    # add a for loop for()
+    ethercrowd.getInvestedFunds(projectId, {'from': account})  # montant qui a ete mit
+
+    # Assert
+    assert expected == result
